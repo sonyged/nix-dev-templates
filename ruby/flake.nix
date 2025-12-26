09817b88102b87264277d76ae544ec9c8209bdb1
Duc Nghiem-Xuan <git@ducnx.com>
2025-12-26 10:19:28 +0900

    Add flake template for ruby env

---
+++ ruby/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A Ruby development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11"; # <- change me to the latest version (new version released in May and November)
+    nixpkgs-ruby = {
+      url = "github:bobvanderlinden/nixpkgs-ruby";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+
+    let
+      overlays = [
+        inputs.nixpkgs-ruby.overlays.default
+        (self: super: { ruby = super."ruby-3.4.8"; }) # <- choose ruby version
+      ];
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system overlays; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [ ruby ];
+          };
+        }
+      );
+    };
+}
